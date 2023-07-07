0fb44181e2c763a39fb6d7c82492b70bd5f8a870
Anna Korshunova <a.i.korshunova@yandex.com>
2023-07-07 03:19:45 +0300

    test2

---
+++ src/test/java/com/example/RestAssuredGetAllureTest.java
@@ -0,0 +1,53 @@
+package com.example;
+
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RestAssuredGetAllureTest {
+
+    String bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZlMzA3ZDI0OGQ0NTAwMzYyN2Y1ZGMiLCJpYXQiOjE2ODMyMzk2NjAsImV4cCI6MTY4Mzg0NDQ2MH0.aN5kqHW1De4FJdMxGYv9LC2pVGBj2ZKDRFS6Sjl_ZxI";
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "https://qa-mesto.praktikum-services.ru";
+    }
+
+    @Test
+    @DisplayName("Check status code of /users/me")
+    public void getMyInfoStatusCode() {
+        given()
+                .auth().oauth2(bearerToken)
+                .get("/api/users/me")
+                .then().statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Check user name")
+    public void checkUserName() {
+        given()
+                .auth().oauth2(bearerToken)
+                .get("/api/users/me")
+                .then().assertThat().body("data.name",equalTo("korshunova_18@gmail.com"));
+    }
+
+    @Test
+    @DisplayName("Check user name and print response body")
+    public void checkUserNameAndPrintResponseBody() {
+
+        Response response =given().auth().oauth2(bearerToken).get("/api/users/me");
+        // отправили запрос и сохранили ответ в переменную response - экземпляр класса Response
+
+        response.then().assertThat().body("data.name",equalTo("korshunova_18@gmail.com"));
+        // проверили, что в теле ответа ключу name соответствует нужное имя пользователя
+
+        System.out.println(response.body().asString()); // вывели тело ответа на экран
+
+    }
+
+}
